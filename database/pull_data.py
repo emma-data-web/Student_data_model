69d463f0fa619194a208435b57afb0fca989397d
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-08-27 18:09:16 +0100

    pulled the data from the database

--- database/pull_data.py
+++ database/pull_data.py
@@ -8,14 +8,19 @@ load_dotenv()
 
 logger = set_logger("pull_logger","logs/logs.txt")
 
-logger.info("it worked")
-
 
 def get_new_data():
-  db_url = f"mysql+pymysql://{os.getenv('db_username')}:{os.getenv('db_password')}@{os.getenv('db_host')}:{os.getenv('db_port')}/{os.getenv('db_database')}"
+  try:
+    logger.info("pulling data from the database")
+    db_url = f"mysql+pymysql://{os.getenv('db_username')}:{os.getenv('db_password')}@{os.getenv('db_host')}:{os.getenv('db_port')}/{os.getenv('db_database')}"
 
-  engine = create_engine(db_url)
-  query = ("select * from `students performance dataset` ")
+    engine = create_engine(db_url)
+    query = ("select * from `students performance dataset`")
 
-  df = pd.read_sql(query, con=engine)
-  return df
+    df = pd.read_sql(query, con=engine)
+    logger.info(f"sucessfully pulled {len(df)}")
+    return df
+  except Exception as e:
+    logger.exception(e)
+    
+
