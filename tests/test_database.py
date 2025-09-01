f1fc54dff2262517a0af6341a1c499b6a83b8c49
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-09-01 12:22:59 +0100

    modifying pytest for db connections

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,6 @@
 from database.pull_data import get_new_data
 import pandas as pd
-from database.pull_data import get_new_data
+from database.pull_data import db_conn
 import pytest
 
 @pytest.fixture
@@ -9,5 +9,20 @@ def data():
   return data.head(2)
 
 
+@pytest.fixture
 def conn():
-  connnection = 
+  connnection = db_conn()
+  return connnection
+
+
+def test_database(data):
+  test_data = data 
+
+  assert len(test_data) == 2
+
+
+def test_columns(data):
+
+  test = data
+
+  assert len(test.columns) > 10
